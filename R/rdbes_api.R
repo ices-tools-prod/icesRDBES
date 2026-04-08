54c095e0b9899ba005802d78f69b521f5799986d
colin millar <colinpmillar@gmail.com>
2026-04-08 16:59:33 +0200

    add production argument

--- R/rdbes_api.R
+++ R/rdbes_api.R
@@ -2,11 +2,13 @@
 #'
 #' This function returns the API URL based on whether the package is in development or production mode.
 #'
+#' @param production Logical. If TRUE, returns the production API URL. If FALSE, returns the development API URL.
+#'
 #' @return A character string representing the API URL.
 #'
 #' @export
-rdbes_api <- function() {
-  if (getOption("rdbes.production")) {
+rdbes_api <- function(production = getOption("rdbes.production")) {
+  if (production) {
     getOption("rdbes.api_prod_url")
   } else {
     getOption("rdbes.api_dev_url")
